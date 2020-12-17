af465f149f228ae20d74c0670010cdb907110fb0
sandycaltechpgp <sandeepkumar.jakkaraju@outlook.com>
2020-12-17 16:19:20 +0530

    clear screen add

--- src/main/java/com/simplilearn/phase1/assessment/DisplayScreen.java
+++ src/main/java/com/simplilearn/phase1/assessment/DisplayScreen.java
@@ -4,14 +4,27 @@ public class DisplayScreen {
 
     private String welcomeText;
 
-    DisplayScreen(String welcomeText){
+    DisplayScreen(String welcomeText) {
         this.welcomeText = welcomeText;
     }
 
-    public void display(){
+    public void display() {
+        clearConsole();
         System.out.println("################################################");
         System.out.println(this.welcomeText);
         System.out.println("################################################");
     }
 
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
